106b86d738174956463c8e61d59afa6591726d97
eoinlynchcodes <eoin95lynch@gmail.com>
2022-11-13 16:48:41 +0000

    added search by fullname functionality

--- frontend/src/components/SearchResultsBox.js
+++ frontend/src/components/SearchResultsBox.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import { Box, Text } from "@chakra-ui/react";
+import React from "react";
+import { Box, Text, Button } from "@chakra-ui/react";
 
-export default function SearchResultsBox({ searchResults, searchQuery }){
+export default function SearchResultsBox({ searchResults, searchQuery }) {
+  console.log({ searchResults });
 
-    console.log({ searchResults });
+  const follow = (userId) => {
+    console.log({ userId });
+  }
 
-    return (
-        <Box p={5} >
-            <Text>Search results for "{searchQuery}"</Text>
+  return (
+    <Box p={5}>
+      <Text>Search results for "{searchQuery}"</Text>
 
-            <Box bgColor="red">
-
-                {
-                    searchResults && 
-                    searchResults?.map((item) => {
-                        return (
-                            <Text>{item.fullName}</Text>
-                        )
-                    })
-                }
-
-            </Box>
-        </Box>
-    );
+      <Box>
+        {searchResults &&
+          searchResults?.map((item) => {
+            return (
+              <Box boxShadow="base" bg="gray.50" m={5} p={5}>
+                <Text>{item.fullName}</Text>
+                <Text>{item.username}</Text>
+                <Text>{item?.location}</Text>
+                <Button onClick={() => follow(item.userId)}>Follow</Button>
+              </Box>
+            );
+          })}
+      </Box>
+    </Box>
+  );
 }
